c6a2a44e6fa4f4019c4b3848ed52ad6e3ee43cea
Gursahib Singh <gursahibsingh@Gursahibs-MacBook-Pro.local>
2019-11-25 02:05:53 -0500

    made changes, added required comments and file header

--- tm4c123gh6pm.h
+++ tm4c123gh6pm.h
@@ -1,12 +1,24 @@
-/*
- * tm4c123gh6pm.h
- *
- *  Created on: Nov 25, 2019
- *  Author: Gursahib Singh
- */
+//----------------------------------------------------------------------------
+// FILE         : TM4C123GH6PM.H
+// FILE VERSION : 1.0
+// PROGRAMMER   : GURSAHIB SINGH
+//----------------------------------------------------------------------------
+// REVISION HISTORY
+//----------------------------------------------------------------------------
+//
+// 1.0, 2019-11-25, GURSAHIB SINGH
+//   - Initial release
+//
+//----------------------------------------------------------------------------
+// INCLUSION LOCK
+//----------------------------------------------------------------------------
+
 #ifndef TM4C123GH6PM_H_
 #define TM4C123GH6PM_H_
 
+//----------------------------------------------------------------------------
+// CONSTANTS
+//----------------------------------------------------------------------------
 #define HWREG(x)                (*((volatile uint32_t*)(x)))
 
 #define SYSCTL_RCGCGPIO         0x400FE608          //GPIO Run Mode Clock Gating Control Register Address
@@ -16,8 +28,8 @@
 #define GPIO_O_DEN              0x0000051C          // Digital Enable Register Offset
 #define GPIO_O_DIR              0x00000400          // Direction Register Offset
 
-#define NVIC_ST_CTRL    0xE000E010                  // SysTick Control and Status Register
-#define NVIC_ST_RELOAD  0xE000E014                  // SysTick Reload Register
-#define NVIC_ST_CURRENT 0xE000E018                  // SysTick Current Register
-
 #endif /* TM4C123GH6PM_H_ */
+
+//----------------------------------------------------------------------------
+// END TM4C123GH6PM.H
+//----------------------------------------------------------------------------
